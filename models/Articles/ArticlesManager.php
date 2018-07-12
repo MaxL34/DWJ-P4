3e2bb1b691621f13e2d2a672a3b4947458e43bf6
MaxL34 <mlauvergeat@gmail.com>
2018-07-12 12:31:58 +0200

    Ajout de la fonction de modification d'un article (update)

--- models/Articles/ArticlesManager.php
+++ models/Articles/ArticlesManager.php
@@ -7,15 +7,28 @@ class ArticlesManager {
         $this->setDb($db);
     }
 
-    public function addArticle(Articles $article) {
-        $q=$this->_db->prepare('INSERT INTO articles (art_id, art_title, art_content, art_author, art_creation_date) VALUES (:art_id, :art_title, :art_content, :art_author, :NOW())');
+    public function addArticle(Article $article) {
+        $q = $this->_db->prepare('INSERT INTO articles (art_id, art_title, art_content, art_author, art_creation_date) VALUES (:art_id, :art_title, :art_content, :art_author, :NOW())');
 
         $q->bindValue(':art_id', $article->art_id());
         $q->bindValue(':art_title', $article->art_title());
         $q->bindValue(':art_content', $article->art_content());
         $q->bindValue(':art_author', $article->art_author());
         $q->bindValue(':art_creation_date', $article->NOW());
+
+        $q->execute();
     }
 
-    
+    public function updateArticle(Article $article) {
+        $q = $this->_db->prepare('UPDATE articles SET art_title = :art_title, art_content = :art_content, art_modified_date = :NOW() WHERE art_id = :art_id');
+
+        $q->bindValue(':art_title', $article->art_title());
+        $q->bindValue(':art_content', $article->art_content());
+        $q->bindValue(':art_modified_date', $article->NOW());
+        $q->bindValue(':art_id', $article->art_id());
+
+        $q->execute();
+    }
+
+
 }
