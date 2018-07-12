6e204364ca31dfadf0280109bc4a048dc5219c27
MaxL34 <mlauvergeat@gmail.com>
2018-07-12 11:33:48 +0200

    Renommage en Articles.php

---
+++ models/Articles/Articles.php
@@ -0,0 +1,52 @@
+<?php
+class ArticlesList {
+    
+    // Décalaration des attributs
+    private $_art_id;
+    private $_art_title;
+    private $_art_content;
+    private $_art_author;
+
+    // Déclaration des getters (accesseurs)
+    public function art_id() {
+        return $this->_art_id;
+    }
+
+    public function art_title() {
+        return $this->_art_title;
+    }
+
+    public function art_content() {
+        return $this->_art_content;
+    }
+
+    public function art_author() {
+        return $this->_art_author;
+    }
+
+    // Déclaration des setters (mutateurs)
+    public function setArt_id($art_id) {
+        $art_id = (int) $art_id;
+        if ($art_id > 0) {
+            $this->_art_id = $art_id;
+        }
+    }
+
+    public function setArt_title($art_title) {
+        if (is_string($art_title)) {
+            $this->_art_title = $art_title;
+        } 
+    }
+
+    public function setArt_content($art_content) {
+        if (is_string($art_content)) {
+            $this->_art_content = $art_content;
+        }
+    }
+
+    public function setArt_author($art_author) {
+        if (is_string($art_author)) {
+            $this->_art_author = $art_author;
+        }
+    }
+}
