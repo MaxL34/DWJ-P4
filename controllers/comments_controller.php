ae18190771e8e605bc9c0256b251184aff8bf999
MaxL34 <mlauvergeat@gmail.com>
2018-07-16 15:24:57 +0200

    Renommage du fichier 'index_controller.php' en 'comments_controller.php'

---
+++ controllers/comments_controller.php
@@ -0,0 +1,9 @@
+<?php
+require('../models/Comments/CommentsManager.php');
+require('../models/Comments/Comment.php');
+
+$db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', 'Jmc@Mysql!');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$commentManager = new CommentsManager($db);
+$comments = $commentManager->listComments();
+require('../views/frontend/indexView.php');
