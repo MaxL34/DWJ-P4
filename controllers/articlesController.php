0696e9f100b2a17760a99ec6da25e576519908e8
MaxL34 <mlauvergeat@gmail.com>
2018-07-17 15:38:23 +0200

    Renommage du fichier 'articles_controller' en 'articlesController' (nomenclature camelCase)

---
+++ controllers/articlesController.php
@@ -0,0 +1,11 @@
+<?php
+require('../models/Articles/ArticlesManager.php');
+require('../models/Articles/Article.php');
+
+$db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', 'Jmc@Mysql!');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$articlesManager = new ArticlesManager($db);
+$articles = $articlesManager->listArticles();
+$post = $articlesManager->commentsFromArticles();
+require('../views/frontend/articleView.php');
+require('../views/frontend/postView.php');
