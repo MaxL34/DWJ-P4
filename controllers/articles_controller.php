6ba3107b1e01ae38efad20c2accd7d9633e9854a
MaxL34 <mlauvergeat@gmail.com>
2018-07-17 14:48:26 +0200

    Ecriture en cours du code pour tests d'affichage des commentaires associés à un billet donné

--- controllers/articles_controller.php
+++ controllers/articles_controller.php
@@ -6,4 +6,6 @@ $db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', 'Jmc@M
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $articlesManager = new ArticlesManager($db);
 $articles = $articlesManager->listArticles();
+$post = $articlesManager->commentsFromArticles();
 require('../views/frontend/articleView.php');
+require('../views/frontend/postView.php');
---
+++ views/frontend/postView.php
@@ -0,0 +1,23 @@
+<?php $title = 'Blog de Jean Forteroche : Article et commentaires associés'; ?>
+
+<?php ob_start(); ?>
+<h1>Billet simple pour l'Alaska</h1>
+<p>Billet <?php echo $articles->art_title(); ?> et ses commentaires</p>
+
+<?php
+    foreach ($articles as $value) {
+?>
+    <div>
+        <p>
+            <?php echo $value->art_title(); ?></br>
+            <?php echo $value->art_content(); ?></br>
+            <?php echo $value->art_author(); ?></br>
+        </p>
+        <a href="../views/frontend/postView.php">Afficher le billet et ses commentaires</a>
+    </div>
+<?php
+    }
+?>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('frontTemplate.php'); ?>
