43652d21136991b71b3555291b1ed3bd0a442226
MaxL34 <mlauvergeat@gmail.com>
2018-07-19 09:12:30 +0200

    Ecriture du code du controller principal (Ajout des fonctions 'listArticles' et 'getArticle')

---
+++ controllers/controller.php
@@ -0,0 +1,21 @@
+<?php
+require('../models/Articles/Article.php');
+require('../models/Articles/ArticlesManager.php');
+
+function listArticles() {
+    $db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', 'Jmc@Mysql!');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $articlesManager = new ArticlesManager($db);    
+    $articles = $articlesManager->listArticles();
+
+    require('../views/frontend/articleView.php');
+}
+
+function getArticle() {
+    $db = new PDO('mysql:host=localhost;dbname=blog_jf;charset=utf8', 'root', 'Jmc@Mysql!');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    $articlesManager = new ArticlesManager($db);
+    $article = $articlesManager->getArticle($_GET['article_id']);
+
+    require('../views/frontend/postView.php');
+}
