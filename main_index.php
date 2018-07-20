cdd192de0e4c34c97bcdbc78e5b5bde52593bd23
MaxL34 <mlauvergeat@gmail.com>
2018-07-20 16:59:35 +0200

    Création du fichier pour tests implémentation routeur

---
+++ main_index.php
@@ -0,0 +1,14 @@
+<?php
+require('controllers/controller.php');
+
+if (isset($_GET['action']) && ($_GET['action'] === 'getArticle')) {
+    if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
+            getArticle();
+    }
+    else {
+        echo 'Erreur d\'id d\'article : aucun id envoyé ou id inexistant';
+    }
+}
+else {
+    listArticles();
+}
