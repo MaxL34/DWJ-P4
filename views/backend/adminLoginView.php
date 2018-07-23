b2a1dfc0e163fe1c8d44434551f0c9ae48d12ec9
MaxL34 <mlauvergeat@gmail.com>
2018-07-23 17:32:41 +0200

    Ecriture du code de la page de connexion à l'administration

---
+++ views/backend/adminLoginView.php
@@ -0,0 +1,28 @@
+<?php $title = 'Blog de Jean Forteroche : Connexion à l\'espace d\'administration'; ?>
+
+<?php ob_start(); ?>
+<h1>Billet simple pour l'Alaska</h1>
+<p>Connexion à l'espace d'administration du blog</p>
+
+<p>Veuillez entrer vos identifiants de connexion pour accéder à l'administration du blog</p>
+
+<div>
+    <form action="../../main_index.php?action=adminLogin" method="post">
+        <div>
+            <label for="user">Login : </label>
+            <input type="text" id="user_login" name="user" />
+        </div>
+
+        <div>
+            <label for="password">Mot de passe : </label>
+            <input type="text" id="user_password" name="password" />
+        </div>
+
+        <div>
+            <input type="submit" value="Se connecter" />
+        </div>
+    </form>
+</div>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('../frontend/frontTemplate.php'); ?>
