b695623788c22181b742beaa53cecb63d247dc55
MaxL34 <mlauvergeat@gmail.com>
2018-07-24 18:05:26 +0200

    Création de la vue pour l'inscription à l'espace membres

---
+++ views/frontend/subscriptionView.php
@@ -0,0 +1,23 @@
+<?php $title = 'Blog de Jean Forteroche : Inscription à l\'espace membre'; ?>
+
+<?php ob_start(); ?>
+<h1>Billet simple pour l'Alaska</h1>
+<p>Page d'inscription à l'espace membre</p>
+
+<?php
+    foreach ($articles as $value) {
+?>
+    <div>
+        <p>
+            <?php echo $value->art_title(); ?></br>
+            <?php echo $value->art_content(); ?></br>
+            <?php echo $value->art_author(); ?></br>
+        </p>
+        <a href="main_index.php?action=getArticle&amp;article_id=<?php echo $value->art_id(); ?>">Afficher le billet et ses commentaires</a>
+    </div>
+<?php
+    }
+?>
+
+<?php $content = ob_get_clean(); ?>
+<?php require('views/layout.php'); ?>
