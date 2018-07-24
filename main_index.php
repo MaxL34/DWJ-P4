467a54d83e59d491dcf5525e114b849735222829
MaxL34 <mlauvergeat@gmail.com>
2018-07-24 16:59:33 +0200

    Modification du code, utilisation de la condition switch : tests OK

--- main_index.php
+++ main_index.php
@@ -1,38 +1,43 @@
 <?php
 require('./controllers/controller.php');
 
-if (isset($_GET['action'])) {
-    if ($_GET['action'] == 'getArticle') {
-        if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
-            getArtCom();
-        } else {
-            echo 'Erreur d\'id d\'article : aucun id envoyé ou id inexistant';
-          }
-    } elseif ($_GET['action'] == 'addComment') {
-        if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
-            if (!empty($_POST['com_content']) && !empty($_POST['com_author'])) {
-                addComment($_POST['com_content'], $_POST['com_author'], $_GET['article_id']);
+if (!empty($_GET['action'])) {
+    switch ($_GET['action']) {
+        case 'getArticle':
+            if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
+                getArtCom();
             } else {
-                echo 'Erreur : tous les champs ne sont pas remplis !';
-              }
-        } else {
-            echo 'Erreur : aucun identifiant de billet envoyé';
-          }
-      } elseif ($_GET['action'] == 'adminLogin') {
-          if (!empty($_POST['user']) && (!empty($_POST['password']))) {
-              logUser($_POST['user'], $_POST['password']);
+                echo 'Erreur d\'id d\'article : aucun id envoyé ou id inexistant';
+            }
+        break;
+        
+        case 'addComment':
+            if (isset($_GET['article_id']) && $_GET['article_id'] > 0) {
+                if (!empty($_POST['com_content']) && !empty($_POST['com_author'])) {
+                    addComment($_POST['com_content'], $_POST['com_author'], $_GET['article_id']);
+                } else {
+                    echo 'Erreur : tous les champs ne sont pas remplis !';
+                }
+            } else {
+                echo 'Erreur : aucun identifiant de billet envoyé';
+            }
+        break;
+
+        case 'adminLogin':
+            if (!empty($_POST['user']) && (!empty($_POST['password']))) {
+                logUser($_POST['user'], $_POST['password']);
             
-              if ($_POST['user'] == 'Admin8434' && $_POST['password'] == 'AdminBl0g_Jf') {
-                header('Location: views/backend/adminBoard.php');
-            } else {
-                echo 'Identifiants de connexion incorrects';
-              }
+                if ($_POST['user'] == 'Admin8434' && $_POST['password'] == 'AdminBl0g_Jf') {
+                    header('Location: views/backend/adminBoard.php');
+                } else {
+                    echo 'Identifiants de connexion incorrects';
+                }
+            }
+        break;
 
-          }
+        default:
+            echo 'Erreur : aucune valeur pour le paramètre "action"';
+    }
+} else {
+        listArticles();
       }
-
-}
-
-else {
-    listArticles();
-}
