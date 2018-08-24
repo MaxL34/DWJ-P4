82efba47676ee292df4273761eb2265b520f8c96
MaxL34 <mlauvergeat@gmail.com>
2018-08-24 13:47:26 +0200

    Suppression de TinyMCE de la page

--- views/backend/articleEditionView.php
+++ views/backend/articleEditionView.php
@@ -9,24 +9,7 @@
     <head>
         <meta charset="utf-8" />
         <title>Création d'un article</title>
-        <link href="style.css" rel="stylesheet" />
-        <script type="text/javascript" src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0ofr5ymckej6ac4mcq2doqoozjut2zifnyjjfp067bqy5s12">// <![CDATA[/tiny_mce/tiny_mce.js">// ]]></script>
-        <script type="text/javascript">// <![CDATA[
-        tinymce.init({
-    selector: '.content',
-    theme: 'modern',
-    width: 1024,
-    height: 300,
-    plugins: [
-      'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
-      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-      'save table contextmenu directionality emoticons template paste textcolor'
-    ],
-    content_css: 'css/content.css',
-    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
-  });
-  </script>
-        
+        <link href="style.css" rel="stylesheet" />        
     </head>
 
     <body>
@@ -46,23 +29,17 @@
         <?php
             foreach ($articles as $value) {
         ?>
-        <div>
-            <p>
-                <?php echo $value->art_title(); ?></br>
-                <?php echo $value->art_content(); ?></br>
-                Publié le <?php echo $value->art_creation_date() . ' par : ' . $value->art_author(); ?></br>
-            </p>
-            <a href="main_index.php?action=editArticle&amp;article_id=<?php echo $value->art_id(); ?>">Modifier le billet</a>
-            <a href="main_index.php?action=deleteArticle&amp;article_id=<?php echo $value->art_id(); ?>">Supprimer le billet</a>
-        </div>
-<?php
-    }
-?>
-        
-        <form action="../../main_index.php?action=addArticle" method="post">
-            <textarea class="content" style="width: 60%;" name="title"></textarea></br>
-            <textarea class="content" style="width: 60%;" name="content"></textarea></br>
-            <input name="art_send" type="submit" value="Envoyer" />
-        </form>
+            <div>
+                <p>
+                    <?php echo $value->art_title(); ?></br>
+                    <?php echo $value->art_content(); ?></br>
+                    Publié le <?php echo $value->art_creation_date() . ' par : ' . $value->art_author(); ?></br>
+                </p>
+                <a href="main_index.php?action=editArticle&amp;article_id=<?php echo $value->art_id(); ?>">Modifier le billet</a>
+                <a href="main_index.php?action=deleteArticle&amp;article_id=<?php echo $value->art_id(); ?>">Supprimer le billet</a>
+            </div>
+        <?php
+            }
+        ?>
     </body>
 </html>
