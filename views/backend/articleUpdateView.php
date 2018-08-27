ca25707a91f2e842934985e399f0e6da0aed1951
MaxL34 <mlauvergeat@gmail.com>
2018-08-27 19:35:08 +0200

    Ajout de la date du publication et de l'auteur

--- views/backend/articleUpdateView.php
+++ views/backend/articleUpdateView.php
@@ -43,18 +43,19 @@
         
             <div>
                 <p>
-                    <?php echo $article['art_title']; ?></br>
-                    <?php echo $article['art_content']; ?></br>
-                    Publié le <?php echo $article['art_creation_date'] . ' par : ' . $article['art_author']; ?></br>
+                    <?php echo $article->art_title(); ?></br>
+                    <?php echo $article->art_content(); ?></br>
+                    Publié le <?php echo $article->date_fr(); ?></br>
+                    Par : <?php echo $article->art_author(); ?>
                 </p>
             </div>
             
         <form action="./main_index.php?action=updateArticle&amp;article_id=<?php echo $_GET['article_id']; ?>" method="post">
             <textarea class="content" style="width: 60%;" name="title">
-                <?php echo $article['art_title']; ?>
+                <?php echo $article->art_title(); ?>
             </textarea></br>
             <textarea class="content" style="width: 60%;" name="content">
-                <?php echo $article['art_content']; ?> 
+                <?php echo $article->art_content(); ?> 
             </textarea></br>
             <input name="art_send" type="submit" value="Envoyer" />
         </form>
