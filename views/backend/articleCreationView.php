2b25e96ef88762ad7ede6370d3b2c46e5c1df71d
MaxL34 <mlauvergeat@gmail.com>
2018-09-03 17:20:37 +0200

    modification du code html (ajout d'une liste) et ajout du lien vers la feuille de style : tests OK

--- views/backend/articleCreationView.php
+++ views/backend/articleCreationView.php
@@ -12,19 +12,24 @@
         <link href="style.css" rel="stylesheet" />
         <script type="text/javascript" src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0ofr5ymckej6ac4mcq2doqoozjut2zifnyjjfp067bqy5s12">// <![CDATA[/tiny_mce/tiny_mce.js">// ]]></script>
         <script type="text/javascript" src="/tests/Openclassrooms/DWJ-P4/public/js/tinymce/default_editor.js"></script>
+        <link rel="stylesheet" href="/tests/Openclassrooms/DWJ-P4/public/css/main.css" />
     </head>
 
     <body>
-        <a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Accueil</a>
-        <?php
-            if (isset($_SESSION['id'])) {
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay">Administration</a>&nbsp';
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut">Déconnexion</a>';
-            } else {
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/views/backend/adminLoginView.php">Administration</a>&nbsp';
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/views/frontend/subscriptionView.php">Inscription</a>';  
-              }
-        ?>
+        <ul class="main_menu">
+                    <li>
+                        <a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Accueil</a>
+                    </li>
+                        <?php
+                            if (isset($_SESSION['id'])) {
+                                echo '<li><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay">Administration</a></li>';
+                                echo '<li><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut">Déconnexion</a></li>';
+                            } else {
+                                echo '<li><a href="/tests/Openclassrooms/DWJ-P4/views/backend/adminLoginView.php">Administration</a></li>';
+                                echo '<li><a href="/tests/Openclassrooms/DWJ-P4/views/frontend/subscriptionView.php">Inscription</a></li>';  
+                            }
+                        ?>
+                </ul>
 
         <form action="../../main_index.php?action=addArticle" method="post">
             <p>Titre du billet :</p>
