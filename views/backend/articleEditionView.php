c63fcd22591dd4fe1b871da86c7d93964923a99f
MaxL34 <mlauvergeat@gmail.com>
2018-09-03 17:14:07 +0200

    ajout lien vers la feuille de style dans la balise head

--- views/backend/articleEditionView.php
+++ views/backend/articleEditionView.php
@@ -9,20 +9,24 @@
     <head>
         <meta charset="utf-8" />
         <title>Création d'un article</title>
-        <link href="style.css" rel="stylesheet" />        
+        <link rel="stylesheet" href="/tests/Openclassrooms/DWJ-P4/public/css/main.css" />        
     </head>
 
     <body>
-        <a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Accueil</a>
-        <?php
-            if (isset($_SESSION['id'])) {
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay">Administration</a>&nbsp';
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut">Déconnexion</a>';
-            } else {
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/views/backend/adminLoginView.php">Administration</a>&nbsp';
-                echo '<a href="/tests/Openclassrooms/DWJ-P4/views/frontend/subscriptionView.php">Inscription</a>';  
-              }
-        ?>
+        <ul class="main_menu">
+                <li>
+                    <a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Accueil</a>
+                </li>
+                    <?php
+                        if (isset($_SESSION['id'])) {
+                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay">Administration</a></li>';
+                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut">Déconnexion</a></li>';
+                        } else {
+                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/views/backend/adminLoginView.php">Administration</a></li>';
+                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/views/frontend/subscriptionView.php">Inscription</a></li>';  
+                        }
+                    ?>
+            </ul>
 
         <p>Quel billet voulez-vous modifier ?</p>
 
