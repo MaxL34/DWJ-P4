6e16a6c61d3c6a20570e0967b533a58f960abb4c
MaxL34 <mlauvergeat@gmail.com>
2018-09-03 17:47:41 +0200

    Ajout d'une div 'container' de manière à pouvoir appliquer des styles : tests OK

--- views/frontend/articleView.php
+++ views/frontend/articleView.php
@@ -4,21 +4,28 @@
 <h1>Billet simple pour l'Alaska</h1>
 <p>Liste des billets du blog</p>
 
+<div class="container">
+
 <?php
     foreach ($articles as $value) {
 ?>
-    <div>
-        <p>
-            <?php echo $value->art_title(); ?>
-            <?php echo $value->art_content(); ?>
-            Publié le <?php echo $value->date_fr(); ?></br>
-            Par : <?php echo $value->art_author(); ?>
-        </p>
-        <a href="main_index.php?action=getArticle&amp;article_id=<?php echo $value->art_id(); ?>">Afficher le billet et ses commentaires</a>
-    </div>
+
+    
+        <div class="article">
+            <p>
+                <?php echo $value->art_title(); ?>
+                <?php echo $value->art_content(); ?>
+                Publié le <?php echo $value->date_fr(); ?></br>
+                Par : <?php echo $value->art_author(); ?>
+            </p>
+            <a href="main_index.php?action=getArticle&amp;article_id=<?php echo $value->art_id(); ?>">Afficher le billet et ses commentaires</a>
+        </div>
+    
 <?php
     }
 ?>
+
+</div>
 
 <?php $content = ob_get_clean(); ?>
 <?php require('views/layout.php'); ?>
