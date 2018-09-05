f42c9beb28168c6eaf7dddaca4dee173ee1d3d4d
MaxL34 <mlauvergeat@gmail.com>
2018-09-05 16:29:30 +0200

    Ajout d'une div pour le titre de class ='title'  et refactoristation du code

--- views/frontend/articleView.php
+++ views/frontend/articleView.php
@@ -1,16 +1,15 @@
 <?php $title = 'Blog de Jean Forteroche : Articles'; ?>
 
 <?php ob_start(); ?>
-<h1>Billet simple pour l'Alaska</h1>
-<p>Liste des billets du blog</p>
+<div class="title">
+    <h1>Billet simple pour l'Alaska</h1>
+        <p>Liste des billets du blog</p>
+</div>
 
 <div class="container">
-
-<?php
-    foreach ($articles as $value) {
-?>
-
-    
+    <?php
+        foreach ($articles as $value) {
+    ?>
         <div class="article">
             <p>
                 <?php echo $value->art_title(); ?>
@@ -21,10 +20,9 @@
             <a href="main_index.php?action=getArticle&amp;article_id=<?php echo $value->art_id(); ?>">Afficher le billet et ses commentaires</a>
         </div>
     
-<?php
-    }
-?>
-
+    <?php
+        }
+    ?>
 </div>
 
 <?php $content = ob_get_clean(); ?>
