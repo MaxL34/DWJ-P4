6528bb0fa73981e505cc88e8fb3e70a270ad2529
MaxL34 <mlauvergeat@gmail.com>
2018-09-13 17:17:33 +0200

    Ajout d'un fonction gérant l'évènement au click sur le bouton d'envoi du formaulaire d'un commentaire et affichage d'un message en fonction du résultat (échec ou succès) : tests NOK

--- public/js/commentAdder.js
+++ public/js/commentAdder.js
@@ -1,6 +1,10 @@
 var commentAdder = {
     addComButton: $('.add_com'),
     comForm: $('.com_form'),
+    comAuthor: $('.com_author'),
+    comContent: $('.com_content'),
+    comSubmitBtn: $('.com_submit_btn'),
+    comSubmitMessageElt: $('.com_submit_message'),
 
     init: function() {
         var self = this;
@@ -12,7 +16,17 @@ var commentAdder = {
             self.comForm.show();
         });
 
-
-
+        self.comSubmitBtn.click(function() {
+            if (self.comAuthor == null && self.comContent == null) {
+                self.comSubmitMessageElt.text('Veuillez renseigner votre Pseudo et un commentaire svp.');
+            } else if (self.comAuthor == null) {
+                self.comSubmitMessageElt.text('Veuillez renseigner votre Pseudo svp.');
+              } else if (self.comContent == null) {
+                self.comSubmitMessageElt.text('Veuillez laisser un commentaire svp.');
+                }
+            else {
+                self.comSubmitMessageElt.text('Votre commentaire a bien été ajouté.');
+            }             
+        });
     }
 };
