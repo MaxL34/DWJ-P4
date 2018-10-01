a263064a3af153ce57f51383caa05cfed2cc829e
MaxL34 <mlauvergeat@gmail.com>
2018-10-01 17:56:29 +0200

    Modificatino du code pour utiliser la variable de session PHP ['user'] : tests NOK

--- public/js/signout.js
+++ public/js/signout.js
@@ -1,17 +1,21 @@
 var signOut = {
-    signOutLink: $('#signOut_link'),
+    signOutLink: $('.signOut_link'),
     sessionUser: $('#session_user'),
+    //session_user: '<?php echo json_encode($_SESSION['user']); ?>',
+    
 
     init: function() {
         var self = this;
+        //console.log(sessionUser);
 
         self.signOutLink.click(function(e) {
             e.preventDefault();
-
+            
             $.ajax({
                 url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut',
                 type: 'GET',
                 success: function() {
+                    console.log(sessionUser);
                     console.log(self.sessionUser.text());
                     alert('A bientôt ' + self.sessionUser.text() + '.');
                     window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php";
