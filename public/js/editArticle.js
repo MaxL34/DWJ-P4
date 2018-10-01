6695354b1eebad97f549aa5aaccd49858c6776ff
MaxL34 <mlauvergeat@gmail.com>
2018-10-01 17:58:58 +0200

    Renommage de l'ancien fichier (deleteArticle.js) en editArticle.js

---
+++ public/js/editArticle.js
@@ -0,0 +1,28 @@
+var artDel = {
+    editBtn: $('.edit'),
+
+    init: function() {
+        var self = this;
+        console.log('init deleteArticle lancé');
+        console.log(artId);
+
+        self.editBtn.click(function(e) {
+            e.preventDefault();
+            console.log('cliqué');
+            console.log(artId);
+
+            $.ajax({
+                url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=deleteArticle',
+                type: 'GET',
+                data: '&article_id=' + artId,
+                dataType: 'text',
+                success: function(data) {
+                    if (data == '1') {
+                        alert('L\'article a bien été supprimé.');
+                        window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php?action=listArticlesToEdit";
+                    }
+                } 
+            });
+        })  ;
+    }
+};
