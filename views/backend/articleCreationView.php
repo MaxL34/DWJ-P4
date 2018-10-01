05c3967bc427b5834d1354f51007472e6afe7c02
MaxL34 <mlauvergeat@gmail.com>
2018-10-01 14:27:03 +0200

    Refactorisation du code

--- views/backend/articleCreationView.php
+++ views/backend/articleCreationView.php
@@ -4,35 +4,12 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Création d'un article</title>
-        <link href="style.css" rel="stylesheet" />
-        <script type="text/javascript" src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0ofr5ymckej6ac4mcq2doqoozjut2zifnyjjfp067bqy5s12">// <![CDATA[/tiny_mce/tiny_mce.js">// ]]></script>
-        <script type="text/javascript" src="/tests/Openclassrooms/DWJ-P4/public/js/tinymce/default_editor.js"></script>
-        <link rel="stylesheet" href="/tests/Openclassrooms/DWJ-P4/public/css/main.css" />
-    </head>
+<?php $title = 'Blog de Jean Forteroche : Création d\'un article'; ?>
 
-    <body>
-        <ul class="main_menu">
-            <li>
-                <a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Accueil</a>
-                </li>
-                    <?php
-                        if (isset($_SESSION['user'])) {
-                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay">Administration</a></li>';
-                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut">Déconnexion</a></li>';
-                        } else {
-                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/views/backend/adminLoginView.php">Administration</a></li>';
-                            echo '<li><a href="/tests/Openclassrooms/DWJ-P4/views/frontend/subscriptionView.php">Inscription</a></li>';  
-                        }
-                    ?>
-        </ul>
-        <div class="article_creation_container">                
-            <h1>Rédaction d'un nouveau billet</h1>
+<?php ob_start(); ?>
 
+    <div class="article_creation_container">                
+        <h1>Rédaction d'un nouveau billet</h1>
             <form class="article_form" action="../../main_index.php?action=addArticle" method="post">
                 <p>Titre du billet :</p>
                     <textarea class="content" name="title"></textarea></br>
@@ -40,6 +17,12 @@
                     <textarea class="content" name="content"></textarea></br>
                 <input name="art_send" type="submit" value="Envoyer" />
             </form>
-        </div>
-    </body>
-</html>
+    </div>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php 
+    if (file_exists('../layout.php')) {
+        require('../layout.php');
+    }
+?>
