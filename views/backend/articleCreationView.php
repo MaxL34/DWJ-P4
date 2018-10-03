789292f795605d59b9f315c1270ba4c663045bf6
MaxL34 <mlauvergeat@gmail.com>
2018-10-03 17:01:14 +0200

    Modifications du code pour test d'affichage alerte utilisateur en cas de succès ou d'échec d'ajout d'un nouveau billet : tests NOK

--- views/backend/articleCreationView.php
+++ views/backend/articleCreationView.php
@@ -1,3 +1,9 @@
+<?php
+    if (!isset($_SESSION)) {
+        session_start();
+    }
+?>
+
 <?php $title = 'Blog de Jean Forteroche : Création d\'un article'; ?>
 
 <?php ob_start(); ?>
@@ -6,10 +12,11 @@
         <h1>Rédaction d'un nouveau billet</h1>
             <form class="article_form" action="../../main_index.php?action=addArticle" method="post">
                 <p>Titre du billet :</p>
-                    <textarea class="content" name="title"></textarea></br>
+                    <textarea class="content" id="art_title" name="title"></textarea></br>
                 <p>Contenu du billet :</p>
-                    <textarea class="content" name="content"></textarea></br>
-                <input name="art_send" type="submit" value="Envoyer" />
+                    <textarea class="content" id="art_content" name="content"></textarea></br>
+                <!-- <input type="hidden" id="session_user" name="user" value=<?php echo $_SESSION['user']; ?> /> -->
+                <input id="art_submit_btn" name="art_send" type="submit" value="Envoyer" />
             </form>
     </div>
 
