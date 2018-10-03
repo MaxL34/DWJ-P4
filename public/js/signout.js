b92821cd13c8eb1b2386d1990b78ebb841b3c5a5
MaxL34 <mlauvergeat@gmail.com>
2018-10-03 13:30:19 +0200

    Suppression des console.log

--- public/js/signout.js
+++ public/js/signout.js
@@ -4,17 +4,14 @@ var signOut = {
     
     init: function() {
         var self = this;
-        console.log('init signOut lancée, user = ' + self.user);
 
         self.signOutLink.click(function(e) {
             e.preventDefault();
-            console.log('click lien, user = ' + self.user);
             
             $.ajax({
                 url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut',
                 type: 'GET',
                 success: function() {
-                    console.log('success function console log : ' + self.user);
                     alert('A bientôt ' + self.user + '.');
                     window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php";
                 }
