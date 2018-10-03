074e5b9e7dc952991c1dc7eca8ecf56390f7dee4
MaxL34 <mlauvergeat@gmail.com>
2018-10-03 13:00:36 +0200

    Utilisation de la variable sUser : tests OK

--- public/js/signout.js
+++ public/js/signout.js
@@ -1,19 +1,21 @@
 var signOut = {
     signOutLink: $('.signOut_link'),
-    sessionUser: $('#session_user'),
-
+    user: sUser,
+    
     init: function() {
         var self = this;
+        console.log('init signOut lancée, user = ' + self.user);
 
         self.signOutLink.click(function(e) {
             e.preventDefault();
+            console.log('click lien, user = ' + self.user);
             
             $.ajax({
                 url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=signOut',
                 type: 'GET',
                 success: function() {
-                    console.log(self.sessionUser.text());
-                    alert('A bientôt ' + self.sessionUser.text() + '.');
+                    console.log('success function console log : ' + self.user);
+                    alert('A bientôt ' + self.user + '.');
                     window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php";
                 }
             });
