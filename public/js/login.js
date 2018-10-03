8be390db1347454a26d5b1c127772179f9f6f590
MaxL34 <mlauvergeat@gmail.com>
2018-10-03 13:54:00 +0200

    Suppresssion de commentaires

--- public/js/login.js
+++ public/js/login.js
@@ -8,7 +8,6 @@ var loginValid = {
         var self = this;
         self.loginBtn.click(function(e) {
             e.preventDefault();
-            //var donnees = $(this).serialize();
 
             $.ajax({                
                 url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminLogin',
@@ -17,14 +16,11 @@ var loginValid = {
                 dataType: 'text',
                 success: function(data) {
                     if (data == 'success') {
-                        //self.loginMessageElt.text('Connexion établie.');
                         alert('Heureux de vous revoir ' + self.userLogin.val() + '.');
                         window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay";
                     } else if (data == 'failed') {
-                        //self.loginMessageElt.text('Echec d\'authentfication : mauvais identifiants.');
                         alert('Echec d\'authentfication : mauvais identifiants.');
                     } else {
-                        //self.loginMessageElt.text('Veuillez renseigner tous les champs requis.');
                         alert('Veuillez renseigner tous les champs requis.');
                     } 
                 }
