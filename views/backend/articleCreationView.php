111d2db1377abab773136d8ec5a6646dbfd87e79
MaxL34 <mlauvergeat@gmail.com>
2018-10-04 14:48:50 +0200

    Ajout des variables servant à tinyMCE (appelées dans le template) : tests OK

--- views/backend/articleCreationView.php
+++ views/backend/articleCreationView.php
@@ -4,14 +4,10 @@
     }
 ?>
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Blog de Jean Forteroche : Création d'un article</title>
-        <script type="text/javascript" src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0ofr5ymckej6ac4mcq2doqoozjut2zifnyjjfp067bqy5s12"></script>
-        <script type="text/javascript" src="/tests/Openclassrooms/DWJ-P4/public/js/tinymce/default_editor.js"></script>
-    </head>
+<?php $title = 'Blog de Jean Forteroche : Création d\'un billet'; ?>
+<?php $tinyMCE_API = 'src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=0ofr5ymckej6ac4mcq2doqoozjut2zifnyjjfp067bqy5s12"'; ?>
+<?php $tinyMCE_DefaultEditor = 'src="/tests/Openclassrooms/DWJ-P4/public/js/tinymce/default_editor.js"'; ?>
+<?php $tinymceInit = 'tinymce.init({ selector:\'content\' });'; ?>
 
 <?php ob_start(); ?>
 
