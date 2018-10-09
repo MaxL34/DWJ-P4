f476b3ef95d89dc6e12c8f5a77cf02bb289270c7
MaxL34 <mlauvergeat@gmail.com>
2018-10-09 15:25:09 +0200

    Modifications du code pour tests sur l'appel AJAX : tests OK

--- public/js/articleAdder.js
+++ public/js/articleAdder.js
@@ -1,27 +1,35 @@
 var articleToAdd = {
     sessionUser: sUser,
-    artTitle: $('#art_title'),
-    artContent: $('#art_content'),
+    //artTitle: tinyMCE.('art_title').getContent(),
+    //artContent: tinyMCE.get('art_content').getContent(),
     artSubmitBtn: $('#art_submit_btn'),
 
     init: function() {
         var self = this;
         console.log('init articleToAdd lancée, user = ' + self.sessionUser);
 
-        self.artSubmitBtn.click(function(e) {
-            e.preventDefault();
 
+
+        self.artSubmitBtn.click('submit', function(e) {
+            e.preventDefault();
+            var artTitle = tinyMCE.get('art_title').getContent();
+            var artContent = tinyMCE.get('art_content').getContent();  
+
+            console.log(artTitle);
+            console.log(artContent);
+            
             $.ajax({
                 url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=addArticle',
                 type: 'POST',
-                data: '&title=' + self.artTitle.val() + '&content=' + self.artContent.val() + '&user=' + self.sessionUser,
+                data: 'title=' + artTitle + '&content=' + artContent + '&user=' + self.sessionUser,
                 dataType: 'text',
                 success: function(data) {
+                    console.log('data=' + data);
                     if (data == 'success') {
-                        console.log('success articleToAdd lancée, user = ' + self.sessionUser + ', titre = ' + self.artTitle.val() + 'contenu = ' + self.artContent.val());
+                        console.log('success articleToAdd lancée, user = ' + self.sessionUser + ', titre = ' + artTitle + 'contenu = ' + artContent);
                         alert('Votre billet a bien été ajouté');
                     } else if (data == 'failed') {
-                        console.log('failed articleToAdd lancée, user = ' + self.sessionUser + ', titre = ' + self.artTitle.val() + 'contenu = ' + self.artContent.val());
+                        console.log('failed articleToAdd lancée, user = ' + self.sessionUser + ', titre = ' + self.artTitle + 'contenu = ' + artContent);
                         alert('Veuillez remplir tous les champs.')
                     }
                 }
