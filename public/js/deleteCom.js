3653d5ffd1c8cb753eb4169eb34ba2919f078fea
MaxL34 <mlauvergeat@gmail.com>
2018-10-16 18:42:59 +0200

    Mise en place du code qui gère la suppression d'un commentaire donné au click sur son bouton associé : tests OK

--- public/js/deleteCom.js
+++ public/js/deleteCom.js
@@ -1,11 +1,36 @@
 var comToDel = {
-    deleteComLink: $('.delete_com_link'),
+    deleteBtn: $('.delete_com_btn'),
 
     init: function() {
         var self = this;
 
-        self.deleteComLink.click(function() {
-            alert('Le commentaire a bien été supprimé.');
+        self.deleteBtn.click(function(e) {
+            e.preventDefault();
+            
+            var comId = $(this).attr("id");
+            var artId = $(this).attr('data_id');
+
+            var confirmResult = confirm('Etes-vous certain de vouloir supprimer ce commentaire ?');
+                if (confirmResult == true) {
+
+                    $.ajax({
+                        url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=deleteCom',
+                        type: 'GET',
+                        data: 'article_id=' + artId + '&com_id=' + comId,
+                        dataType: 'text',
+                        success: function(data) {
+                            console.log('data = ' + data);
+                            if (data == 'success') {
+                                alert('Le commentaire a bien été supprimé.');
+                                window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay";
+                            } else {
+                                console.log('erreur');
+                            }
+                        }
+                    });
+                } else if (confirmResult == false) {
+                    alert('Le commentaire n\'a pas été supprimé'); 
+                }
         });
     }
 };
