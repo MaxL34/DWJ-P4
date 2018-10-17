cdd781e4d77c94b557d6ec4d223de13d3ab4041e
MaxL34 <mlauvergeat@gmail.com>
2018-10-17 16:37:33 +0200

    Mise en place du code pour traitement de l'événement click sur le bouton de suppression d'un commentaire avec la modal (remplacement des alert())

--- public/js/deleteCom.js
+++ public/js/deleteCom.js
@@ -1,36 +1,53 @@
 var comToDel = {
     deleteBtn: $('.delete_com_btn'),
+    modal: $('#modal_delete'),
+    modalText: $('#modal_text'),
+    spanClose: $('.close'),
+    yesModalBtn: $('#yes'),
+    noModalBtn: $('no'),
 
     init: function() {
         var self = this;
 
         self.deleteBtn.click(function(e) {
+            e.stopPropagation();
             e.preventDefault();
-            
+
             var comId = $(this).attr("id");
             var artId = $(this).attr('data_id');
 
-            var confirmResult = confirm('Etes-vous certain de vouloir supprimer ce commentaire ?');
-                if (confirmResult == true) {
+            self.modal.show();
+            
+            self.spanClose.click(function() {
+                self.modal.hide();
+            });
 
-                    $.ajax({
-                        url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=deleteCom',
-                        type: 'GET',
-                        data: 'article_id=' + artId + '&com_id=' + comId,
-                        dataType: 'text',
-                        success: function(data) {
-                            console.log('data = ' + data);
-                            if (data == 'success') {
-                                alert('Le commentaire a bien été supprimé.');
-                                window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay";
-                            } else {
-                                console.log('erreur');
-                            }
+            $(document).click(function(event) { 
+                if(!$(event.target).closest(self.modal).length) {
+                    self.modal.hide();
+                } 
+            });
+
+            self.yesModalBtn.click(function(){
+                $.ajax({
+                    url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=deleteCom',
+                    type: 'GET',
+                    data: 'article_id=' + artId + '&com_id=' + comId,
+                    dataType: 'text',
+                    success: function(data) {
+                        console.log('data = ' + data);
+                        if (data == 'success') {
+                            //alert('Le commentaire a bien été supprimé.');
+                            self.modalText.replaceWith('Le commentaire a bien été supprimé.');
+                            self.modal.fadeOut();
+
+                            window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php?action=adminBoardDisplay";
+                        } else {
+                            console.log('erreur');
                         }
-                    });
-                } else if (confirmResult == false) {
-                    alert('Le commentaire n\'a pas été supprimé'); 
-                }
+                    }
+                });
+            });
         });
     }
 };
