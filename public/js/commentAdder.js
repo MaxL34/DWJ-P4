56ae3c09d03804a8ac175ee8ee7b7645844df174
MaxL34 <mlauvergeat@gmail.com>
2018-10-19 15:29:08 +0200

    Modification du code pour traitement par la modal 'modal_com' au lieu des alertes JS : tests OK

--- public/js/commentAdder.js
+++ public/js/commentAdder.js
@@ -1,15 +1,19 @@
 var commentAdder = {
-    addComButton: $('.add_com'),
-    comForm: $('.com_form'),
-    comAuthor: $('.com_author'),
-    missPseudo: $('.missPseudo'),
-    comContent: $('.com_content'),
-    missCom: $('.missCom'),
-    comSubmitBtn: $('.com_submit_btn'),
-    //comSubmitMessageElt: $('.com_submit_message > p'),
+    addComButton: $('#add_com'),
+    comForm: $('#com_form'),
+    comAuthor: $('#author'),
+    missPseudo: $('#missPseudo'),
+    comContent: $('#content'),
+    artId: $('#art_id').attr('value'),
+    missCom: $('#missCom'),
+    comSubmitBtn: $('#com_submit_btn'),
+    modalCom: $('#modal_com'),
+    modalText: $('#modal_text'),
 
     init: function() {
         var self = this;
+
+        console.log('article_id = ' + self.artId);
 
         //Dissimulation du formulaire d'ajout de commentaire
         self.comForm.hide();
@@ -18,8 +22,26 @@ var commentAdder = {
             self.comForm.show();
         });
 
-        self.comSubmitBtn.click(function() {
-            alert('Votre commentaire à bien été ajouté');
+        self.comSubmitBtn.click(function(e) {
+            e.preventDefault();
+            console.log('article_id = ' + self.artId);
+            console.log('com_content = ' + self.comContent.val() + ', com_author = ' + self.comAuthor.val());
+
+            $.ajax({
+                url: '/tests/Openclassrooms/DWJ-P4/main_index.php?action=addComment',
+                type: 'POST',
+                data: 'com_content=' + self.comContent.val() + '&com_author=' + self.comAuthor.val() + '&art_id=' + self.artId,
+                dataType: 'text',
+                success: function() {
+                    //if (data == 'success') {
+                    self.modalText.text('Votre commentaire à bien été ajouté');
+                    self.modalCom.show();
+                    self.modalCom.fadeOut(3000, function() {
+                        self.modalCom.hide();
+                        window.location.href = "/tests/Openclassrooms/DWJ-P4/main_index.php?action=getArticle&article_id=" + self.artId;
+                    });
+                }
+            });
         });
     }
 };
