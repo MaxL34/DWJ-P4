21ddffc9521ba072df0d14533de488d42c03b071
MaxL34 <mlauvergeat@gmail.com>
2018-10-23 10:01:20 +0200

    Suppression de la ligne 28 (rechargement de la page)

--- public/js/reportCom.js
+++ public/js/reportCom.js
@@ -1,11 +1,33 @@
 var comToReport = {
-    comReportElt: $('.report_com'),
+    comReportBtn: $('.report_com_btn'),
+    //articleId: artId,
+    modalCom: $('#modal_com'),
+    modalText: $('#modal_text'),
 
     init: function() {
         var self = this;
 
-        self.comReportElt.click(function() {
-            alert('Le commentaire a bien été signalé à l\'administrateur');
+        self.comReportBtn.click(function(e) {
+            e.preventDefault();
+
+            var comId = $(this).attr('id');
+            console.log('com_id = ' + $(this).attr('id'));
+            console.log('article_id = ' + artId);
+
+            $.ajax({
+                url: 'main_index.php?action=reportCom',
+                type: 'GET',
+                data: 'article_id=' + artId + '&com_id=' + comId,
+                dataType: 'text',
+                success: function() {
+                    self.modalText.text('Le commentaire a bien été signalé à l\'administrateur');
+                    self.modalCom.show();
+                    self.modalCom.fadeOut(3000, function() {
+                        self.modalText.text('');
+                        self.modalCom.hide();
+                    });
+                }
+            });
         });
     }
 }
