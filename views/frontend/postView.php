25185d8a298403418744a47ec5826093ec87b850
MaxL34 <mlauvergeat@gmail.com>
2018-12-13 16:10:58 +0100

    déplacement des div 'add_com' et du formulaire pour les commentaires

--- views/frontend/postView.php
+++ views/frontend/postView.php
@@ -7,33 +7,19 @@
 <?php $title = 'Blog de Jean Forteroche : Article et commentaires associés'; ?>
 
 <?php ob_start(); ?>
-<div class="title">
+<div class="post_title">
     <h1>Billet simple pour l'Alaska</h1>
     <?php echo $article->art_title(); ?>
 </div>
 
 <div class="postView_article">
-    <p>
-        <?php echo $article->art_content(); ?>
+    <?php echo $article->art_content(); ?>
+</div>
+
+<div class="post_infos">
         Posté le : <?php echo $article->date_fr(); ?></br>
         Par : <?php echo $article->art_author(); ?></br>
         Modifié le <?php echo $article->modified_date_fr(); ?>
-    </p>
-</div>
-
-<div class="comments">
-    <p>Commentaires du billet</p>
-    <?php foreach ($comments as $comment) { ?>
-    <p>
-        Commentaire : <?php echo $comment->com_content(); ?></br>
-        Posté par : <?php echo $comment->com_author(); ?></br>
-        le : <?php echo $comment->com_date_fr(); ?></br>
-        
-        <!-- <a class="report_com" href="main_index.php?action=reportCom&amp;article_id=<?php echo $article->art_id(); ?>&amp;com_id=<?php echo $comment->com_id(); ?>">Signaler</a> -->
-        <input class="report_com_btn" type="button" id="<?php echo $comment->com_id(); ?>" value="Signaler" />
-        
-    </p>
-    <?php } ?>
 </div>
 
 <div id="add_com">
@@ -59,6 +45,21 @@
         <input id="com_submit_btn" type="submit" />
     </div>
 </form>
+
+<div class="comments">
+    <p>Commentaires du billet</p>
+    <?php foreach ($comments as $comment) { ?>
+    <p>
+        Commentaire : <?php echo $comment->com_content(); ?></br>
+        Posté par : <?php echo $comment->com_author(); ?></br>
+        le : <?php echo $comment->com_date_fr(); ?></br>
+        
+        <!-- <a class="report_com" href="main_index.php?action=reportCom&amp;article_id=<?php echo $article->art_id(); ?>&amp;com_id=<?php echo $comment->com_id(); ?>">Signaler</a> -->
+        <input class="report_com_btn" type="button" id="<?php echo $comment->com_id(); ?>" value="Signaler" />
+        
+    </p>
+    <?php } ?>
+</div>
 
 <div id="modal_com" class="modal">
     <div class="modal_content">
