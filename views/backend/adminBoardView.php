391dba2b971ea9bd8660e604e4e6b72d37dec1d7
MaxL34 <mlauvergeat@gmail.com>
2019-01-16 15:08:04 +0100

    Inclusion des div 'admin /title/management/comments' dans une div 'adminBoard_container'

--- views/backend/adminBoardView.php
+++ views/backend/adminBoardView.php
@@ -7,34 +7,36 @@
 <?php $title = 'Blog de Jean Forteroche : Espace d\'administration du blog'; ?>
 
 <?php ob_start(); ?>
-<div class="admin_title">
-    <h1>Espace d'administration</h1>
-    <p>Bienvenue sur votre espace d'administration <span id="session_user"><?php echo $_SESSION['user']; ?></span>.</p>
-    <p>Que souhaitez-vous faire ?</p>
-</div>
+<div class="adminBoard_container">
+    <div class="admin_title">
+        <h1>Espace d'administration</h1>
+        <p>Bienvenue sur votre espace d'administration <span id="session_user"><?php echo $_SESSION['user']; ?></span>.</p>
+        <p>Que souhaitez-vous faire ?</p>
+    </div>
 
-<div class="article_management">
-    <p>Rédiger un nouveau billet : </br><a href="/tests/Openclassrooms/DWJ-P4/views/backend/articleCreationView.php">Rédaction de billets</a></p></br>
-    <p>Editer un billet existant (modifier, supprimer) : </br><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=listArticlesToEdit">Edition de billets</a></p></br>
-    <p>Lire vos billets : </br><a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Lecture de billets</a></p></br>
-</div>
+    <div class="article_management">
+        <p>Rédiger un nouveau billet : </br><a href="/tests/Openclassrooms/DWJ-P4/views/backend/articleCreationView.php">Rédaction de billets</a></p></br>
+        <p>Editer un billet existant (modifier, supprimer) : </br><a href="/tests/Openclassrooms/DWJ-P4/main_index.php?action=listArticlesToEdit">Edition de billets</a></p></br>
+        <p>Lire vos billets : </br><a href="/tests/Openclassrooms/DWJ-P4/main_index.php">Lecture de billets</a></p></br>
+    </div>
 
-<div class="admin_comments">
-    <p>Voici la liste des derniers commentaires signalés :</p>
-        <?php
-            if (isset($var)) {
-                foreach ($var as $value) {
-        ?>
-            <p>
-                Auteur : <?php echo $value->com_author(); ?></br>
-                Commentaire : <?php echo $value->com_content(); ?></br>
-                signalé le : <?php echo $value->com_date_fr() . ', ' . $value->com_report_number() . ' fois.'; ?></br>
-                <button class="delete_com_btn" id="<?php echo $value->com_id(); ?>" data_id="<?php echo $value->article_id(); ?>">Supprimer le commentaire</button>
-            </p>
-        <?php 
-                }
-            } 
-        ?>
+    <div class="admin_comments">
+        <p>Voici la liste des derniers commentaires signalés :</p>
+            <?php
+                if (isset($var)) {
+                    foreach ($var as $value) {
+            ?>
+                <p>
+                    Auteur : <?php echo $value->com_author(); ?></br>
+                    Commentaire : <?php echo $value->com_content(); ?></br>
+                    signalé le : <?php echo $value->com_date_fr() . ', ' . $value->com_report_number() . ' fois.'; ?></br>
+                    <button class="delete_com_btn" id="<?php echo $value->com_id(); ?>" data_id="<?php echo $value->article_id(); ?>">Supprimer le commentaire</button>
+                </p>
+            <?php 
+                    }
+                } 
+            ?>
+    </div>
 </div>
 
 <div id="modal_delete" class="modal">
