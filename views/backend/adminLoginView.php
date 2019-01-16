9d576c5bfcc29c7a7130d169e190ea878b694a48
MaxL34 <mlauvergeat@gmail.com>
2019-01-16 15:09:07 +0100

    Inclusion des div 'title' et 'login_form' dans une div 'login_container'

--- views/backend/adminLoginView.php
+++ views/backend/adminLoginView.php
@@ -1,28 +1,31 @@
 <?php $title = 'Blog de Jean Forteroche : Connexion à l\'espace d\'administration'; ?>
 
 <?php ob_start(); ?>
-<div class="title">
-    <h1>Billet simple pour l'Alaska</h1>
-    <p>Connexion à l'espace d'administration du blog</p>
-    <p>Veuillez entrer vos identifiants de connexion pour accéder à l'administration du blog</p>
-</div>
 
-<div>
-    <form id="login_form" action="../../main_index.php?action=adminLogin" method="post">
-        <div>
-            <label for="user">Login : </label>
-            <input type="text" id="user_login" name="user" required />
-        </div>
+<div class="login_container">
+    <div class="title">
+        <h1>Billet simple pour l'Alaska</h1>
+        <p>Connexion à l'espace d'administration du blog</p>
+        <p>Veuillez entrer vos identifiants de connexion pour accéder à l'administration du blog</p>
+    </div>
 
-        <div>
-            <label for="password">Mot de passe : </label>
-            <input type="password" id="user_password" name="password" required />
-        </div>
+    <div>
+        <form id="login_form" action="../../main_index.php?action=adminLogin" method="post">
+            <div>
+                <label for="user">Login : </label>
+                <input type="text" id="user_login" name="user" required />
+            </div>
 
-        <div>
-            <input id="login_btn" type="submit" value="Se connecter" />
-        </div>
-    </form>
+            <div>
+                <label for="password">Mot de passe : </label>
+                <input type="password" id="user_password" name="password" required />
+            </div>
+
+            <div>
+                <input id="login_btn" type="submit" value="Se connecter" />
+            </div>
+        </form>
+    </div>
 </div>
 
 <div id="modal_login" class="modal">
