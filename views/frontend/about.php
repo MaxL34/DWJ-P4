fdfcd0cb09c27f11ead96349b242756b42e4f27b
MaxL34 <mlauvergeat@gmail.com>
2019-04-23 11:21:22 +0200

    Mise en place du code et insertion du portrait de Jean Forteroche

--- views/frontend/about.php
+++ views/frontend/about.php
@@ -4,17 +4,18 @@
 
 <!-- Header -->
 <header class="masthead" style="background-image: url('img/home-bg.jpg')">
-            <div class="overlay"></div>
-            <div class="container">
-            <div class="row">
-                <div class="col-lg-8 col-md-10 mx-auto">
-                <div class="site-heading">
-                    <h1>Billet Simple pour l'Alaska</h1>
-                    <span class="subheading">A propos de moi<span>
-                </div>
-                </div>
-            </div>
-            </div>
+  <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+              <h1>Billet Simple pour l'Alaska</h1>
+              <span class="subheading">A propos de moi<span>
+          </div>
+        </div>
+      </div>
+      <img id="jf_portrait" src="img/jf_portrait.png">
+    </div>
 </header>
 
   <!-- Main Content -->
@@ -30,10 +31,4 @@
 
 <?php $content = ob_get_clean(); ?>
 
-<?php
-if (file_exists('../layout.php')) {
-    require('../layout.php');
-  }
-?>
-
-<!-- <?php require('views/layout.php'); ?> -->
+<?php require('views/layout.php'); ?>
