aa6a5169a3ca51e840f2bde872e9e161c37b04b1
MaxL34 <mlauvergeat@gmail.com>
2019-04-23 16:04:57 +0200

    Modification de l'extension de l'image de fond

--- views/frontend/postView.php
+++ views/frontend/postView.php
@@ -9,7 +9,7 @@
 <?php ob_start(); ?>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+  <header class="masthead" style="background-image: url('img/post-bg.png')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
